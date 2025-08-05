4a4145987b3db110c186fc6238694dfef79380c7
RL_Khushnihal <ansari.khushnihal@gmail.com>
2025-08-05 11:31:51 +0530

    Added first test comment in HomeController

--- Employee_Portal/Controllers/HomeController.cs
+++ Employee_Portal/Controllers/HomeController.cs
@@ -4,6 +4,8 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Employee_Portal.Controllers
 {
+    // Hello....
+
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
